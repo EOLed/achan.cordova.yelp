eb0461a9147683af27bd550b339619929da3cb5d
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-09-26 01:38:21 +0200

    fixed yelp errors

--- karma.conf.js
+++ karma.conf.js
@@ -1,31 +1,25 @@
 module.exports = function(config) {
   config.set({
-    // base path, that will be used to resolve files and exclude
     basePath: 'src',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
     frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
     files: [
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/components/**/*.js'
     ],
 
     reporters: ['progress'],
 
-    // list of files / patterns to exclude
     exclude: [],
 
-    // web server port
     port: 8080,
 
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
 
     // Start these browsers, currently available:
     // - Chrome
@@ -37,9 +31,6 @@ module.exports = function(config) {
     // - IE (only Windows)
     browsers: ['PhantomJS'],
 
-
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
     singleRun: false
   });
 };
--- src/yelp.js
+++ src/yelp.js
@@ -1,24 +1,26 @@
-angular.module('achan.cordova.yelp', []).provider('yelp', function () {
+"use strict";
+
+angular.module('achan.cordova.yelp', []).provider('yelp', function() {
   var baseUrl = 'http://api.yelp.com/v2';
   var credentials = {};
-  this.identify = function (creds) {
+  this.identify = function(creds) {
     credentials = creds;
   };
 
-  var search = function (services) {
+  var search = function(services) {
     var $http = services.$http,
-        $q = services.$q;
+    $q = services.$q;
 
-    return function (parameters) {
+    return function(parameters) {
       var deferred = $q.defer(),
-          httpMethod = 'GET',
-          url = baseUrl + '/search';
+      httpMethod = 'GET',
+      url = baseUrl + '/search';
 
-      $http.get(baseUrl + '/search', {
+      $http.jsonp(url, {
         params: buildSearchParameters(httpMethod, url, parameters)
-      }).success(function (data) {
+      }).success(function(data) {
         deferred.resolve(data);
-      }).error(function (error) {
+      }).error(function(error) {
         deferred.reject(error);
       });
 
@@ -28,12 +30,7 @@ angular.module('achan.cordova.yelp', []).provider('yelp', function () {
 
   function buildSearchParameters(httpMethod, url, params) {
     params = identifiedParams(params);
-    params.oauth_signature = oauthSignature.generate(httpMethod,
-                                                     url,
-                                                     params,
-                                                     credentials.consumerSecret,
-                                                     credentials.tokenSecret);
-
+    params.oauth_signature = oauthSignature.generate(httpMethod, url, params, credentials.consumerSecret, credentials.tokenSecret);
     return params;
   }
 
@@ -44,13 +41,14 @@ angular.module('achan.cordova.yelp', []).provider('yelp', function () {
     params.oauth_nonce = timestamp;
     params.oauth_timestamp = timestamp;
     params.oauth_signature_method = 'HMAC-SHA1';
+    params.callback = 'angular.callbacks._0';
 
     return params;
   }
 
-  this.$get = function ($http, $q) {
+  this.$get = function($http, $q) {
     return {
-      search: search({ $http: $http, $q: $q })
+      search: search({$http: $http, $q: $q})
     };
   };
 });
